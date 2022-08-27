d98dff49e9ee5b0e2454bcff66c6f9041098faa6
vishal035 <kumarvishal880288@gmail.com>
2022-08-27 14:25:52 +0530

    Home Page ✅

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -5,19 +5,19 @@ const HeroSection = () => {
   return (
     <main className="z-10 mt-8 w-full h-full flex flex-col items-center">
       <section className="ralative h-2/4 w-full flex flex-col items-center text-white ">
-        <h1 className="relative top-4 font-black text-6xl tracking-wider">
+        <h1 className="relative top-4 font-black text-6xl tracking-wider md:text-8xl md:tracking-widest">
           Hi
         </h1>
-        <h2 className="relative top-8 font-bold text-3xl tracking-widest">
+        <h2 className="relative top-8 font-bold text-3xl md:text-6xl tracking-widest md:tracking-[10px]">
           I&apos;m Vishal Kumar
         </h2>
-        <h3 className="relative top-8 text-4xl font-bold tracking-widest">
+        <h3 className="relative top-10 text-4xl md:text-6xl font-bold tracking-widest md:tracking-[10px]">
           A Self Taught
         </h3>
-        <h4 className="relative top-16 text-4xl font-semibold tracking-normal">
+        <h4 className="relative top-16 text-4xl md:text-6xl font-semibold tracking-normal md:tracking-wider">
           FullStack Developer
         </h4>
-        <p className="relative top-16 tracking-normal">
+        <p className="relative top-16 tracking-normal md:tracking-[7px]">
           on the way to become Senior of MySelf 🔥
         </p>
       </section>
