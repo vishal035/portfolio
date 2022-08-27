8311b21ec58fd0e2f710198babad72e1fc5b7470
vishal035 <kumarvishal880288@gmail.com>
2022-08-27 10:54:05 +0530

    DaisyUI Removed

--- components/Header/Navbar/index.tsx
+++ components/Header/Navbar/index.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 const NavBar = () => {
   return (
     <>
-      <div className="navbar bg-primary-black">
+      <div className="min-h-4 flex items-center p-4">
         <div className="flex-1">
-          <a href="#" className=" text-primary uppercase text-xl">
+          <a href="#" className=" p-2 text-primary uppercase text-xl font-bold">
             VK
           </a>
         </div>
         <div className="flex-none">
-          <button className="btn btn-square btn-ghost bg-primary-black text-primary">
+          <button className="px-4 py-2 bg-primary-black text-primary">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
